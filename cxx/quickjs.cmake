aeda79cf0dc78d28d012fe6feeef11515a3124b4
wgh19 <nyne19710@proton.me>
2024-04-18 12:56:21 +0800

    update android build

--- cxx/quickjs.cmake
+++ cxx/quickjs.cmake
@@ -1,6 +1,6 @@
-cmake_minimum_required(VERSION 3.7 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.10)
 set(CXX_LIB_DIR ${CMAKE_CURRENT_LIST_DIR})
-set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD 17)
 
 # quickjs
 set(QUICK_JS_LIB_DIR ${CXX_LIB_DIR}/quickjs)
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_qjs
+import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterQjsPlugin.register(with: registry.registrar(forPlugin: "FlutterQjsPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
